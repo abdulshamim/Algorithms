d51e813d63bf0f49633d4c8dd7e935806d0c819d
Abdul <abdul.shamim@click-labs.com>
2018-03-15 19:01:41 +0530

    Insertion Sort Algo added

--- Algorithms.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Insertion Sort.xcplaygroundpage/Contents.swift
@@ -1,5 +1,24 @@
 //: [Previous](@previous)
 
-import Foundation
+/// Sort the array by Isertion sort
+///
+/// - Parameters:
+///   - arr: array which is to be sort
+///   - isOrder: order in which to be sort the array
+func insertionSort(_ arr: [Int]) {
+    var newArray = arr
+    for i in 1..<newArray.count {
+        var y = i
+        let temp = newArray[y]
+        while y > 0 && temp <  newArray[y-1] {
+            newArray[y] = newArray[y-1]
+            y -= 1
+        }
+        newArray[y] = temp
+    }
+    print(newArray)
+}
 
+var array = [2,1,9,6,3,7,15,23,4,8,12, 12, 5]
+insertionSort(array)
 
